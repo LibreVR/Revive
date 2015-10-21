8f50d5c722ef0a2b1cc109d1f797b1346b8b7683
LibreVR <librevr@users.noreply.github.com>
2015-10-21 16:29:00 -0700

    REV_Assert: Use a unicode string.

--- REV_Assert.h
+++ REV_Assert.h
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <crtdbg.h>
 
-#define REV_UNIMPLEMENTED _ASSERT_EXPR(false, "Function not yet revived.")
+#define REV_UNIMPLEMENTED _ASSERT_EXPR(false, L"Make VR compatible again.")
 #define REV_UNIMPLEMENTED_STRUCT(s) REV_UNIMPLEMENTED; ##s stub; memset(&stub, 0, sizeof(stub)); return stub;
 #define REV_UNIMPLEMENTED_NULL REV_UNIMPLEMENTED; return NULL;
 #define REV_UNIMPLEMENTED_RUNTIME REV_UNIMPLEMENTED; return ovrError_RuntimeException;
