aa6ac5d8ea3b71d9118a9b466923de21287b1d87
LibreVR <librevr@users.noreply.github.com>
2015-10-21 16:29:00 -0700

    Implement audio device query functions.

--- REV_CAPI_Audio.cpp
+++ REV_CAPI_Audio.cpp
@@ -1,15 +1,48 @@
 #include "OVR_CAPI_Audio.h"
 
+#include <Mmddk.h>
+#include <DSound.h>
+
 #include "REV_Assert.h"
 
-OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceOutWaveId(UINT* deviceOutId) { REV_UNIMPLEMENTED_RUNTIME; }
+// TODO: Return the vive-specific audio devices instead of the default.
 
-OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceInWaveId(UINT* deviceInId) { REV_UNIMPLEMENTED_RUNTIME; }
+OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceOutWaveId(UINT* deviceOutId)
+{
+	waveOutMessage((HWAVEOUT)WAVE_MAPPER, DRVM_MAPPER_PREFERRED_GET, (DWORD_PTR)deviceOutId, NULL);
+	return ovrSuccess;
+}
 
-OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceOutGuidStr(WCHAR deviceOutStrBuffer[OVR_AUDIO_MAX_DEVICE_STR_SIZE]) { REV_UNIMPLEMENTED_RUNTIME; }
+OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceInWaveId(UINT* deviceInId)
+{
+	waveInMessage((HWAVEIN)WAVE_MAPPER, DRVM_MAPPER_PREFERRED_GET, (DWORD_PTR)deviceInId, NULL);
+	return ovrSuccess;
+}
 
-OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceOutGuid(GUID* deviceOutGuid) { REV_UNIMPLEMENTED_RUNTIME; }
+OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceOutGuidStr(WCHAR deviceOutStrBuffer[OVR_AUDIO_MAX_DEVICE_STR_SIZE])
+{
+	GUID guid;
+	GetDeviceID(&DSDEVID_DefaultPlayback, &guid);
+	StringFromGUID2(guid, deviceOutStrBuffer, OVR_AUDIO_MAX_DEVICE_STR_SIZE);
+	return ovrSuccess;
+}
 
-OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceInGuidStr(WCHAR deviceInStrBuffer[OVR_AUDIO_MAX_DEVICE_STR_SIZE]) { REV_UNIMPLEMENTED_RUNTIME; }
+OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceOutGuid(GUID* deviceOutGuid)
+{
+	GetDeviceID(&DSDEVID_DefaultPlayback, deviceOutGuid);
+	return ovrSuccess;
+}
 
-OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceInGuid(GUID* deviceInGuid) { REV_UNIMPLEMENTED_RUNTIME; }
+OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceInGuidStr(WCHAR deviceInStrBuffer[OVR_AUDIO_MAX_DEVICE_STR_SIZE])
+{
+	GUID guid;
+	GetDeviceID(&DSDEVID_DefaultCapture, &guid);
+	StringFromGUID2(guid, deviceInStrBuffer, OVR_AUDIO_MAX_DEVICE_STR_SIZE);
+	return ovrSuccess;
+}
+
+OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetAudioDeviceInGuid(GUID* deviceInGuid)
+{
+	GetDeviceID(&DSDEVID_DefaultCapture, deviceInGuid);
+	return ovrSuccess;
+}
