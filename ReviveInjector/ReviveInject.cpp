57b42774bc00fb6f3ed579fa6082c2842c151025
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2016-04-12 02:38:22 -0400

    Make LibRevive32 and LibRevive64 play nice together (2nd attempt)

--- ReviveInjector/ReviveInject.cpp
+++ ReviveInjector/ReviveInject.cpp
@@ -6,6 +6,10 @@
 #include <direct.h>
 #include <stdlib.h>
 
+bool InjectLibRevive(HANDLE processHandle, HANDLE threadHandle);
+bool InjectOpenVR(HANDLE processHandle, HANDLE threadHandle);
+bool InjectDLL(HANDLE processHandle, HANDLE threadHandle, const char *dllPath, int dllPathLength);
+
 const LPWSTR GetLPWSTR(char *c)
 {
     const size_t cSize = strlen(c)+1;
@@ -14,85 +18,97 @@ const LPWSTR GetLPWSTR(char *c)
     return wc;
 }
 
-int CreateProcessAndInject(char *programPath)
-{
-	char *cwd;
-	if ((cwd = _getcwd(NULL, 0)) == NULL) {
-		printf("Failed to get cwd\n");
-		return -1;
-	}
-
+int CreateProcessAndInject(char *programPath) {
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
 	ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
-	// TODO: close the created process if something goes wrong
 	if (!CreateProcess(NULL, GetLPWSTR(programPath), NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi))
 	{
 		printf("Failed to create process\n");
 		return -1;
+	}	
+	if (!InjectOpenVR(pi.hProcess, pi.hThread) ||
+		!InjectLibRevive(pi.hProcess, pi.hThread)) {
+		return -1;
 	}
+	printf("Injected dlls succesfully\n");
+	ResumeThread(pi.hThread);
+	return 0;
+}
 
-	const char *dllName = "LibRevive32_1.dll";
-	const char *platform = "x86";
+bool InjectDLL(HANDLE processHandle, HANDLE threadHandle, const char *dllName32, const char *dllName64) {
+	char *cwd;
+	if ((cwd = _getcwd(NULL, 0)) == NULL) {
+		printf("Failed to get cwd\n");
+		return -1;
+	}
+	BOOL is32Bit = TRUE;
 #if _WIN64
-	BOOL b32bit = FALSE;
-	if (!IsWow64Process(pi.hProcess, &b32bit))
+	if (!IsWow64Process(processHandle, &is32Bit))
 	{
 		printf("Failed to query bit depth\n");
 		return -1;
 	}
-	if (!b32bit) {
-		dllName = "LibRevive64_1.dll";
-		platform = "x64";
-	}
 #endif
+	char *platform = is32Bit ? "x86" : "x64";
+	const char *dllName = is32Bit ? dllName32 : dllName64;
 	char dllPath[MAX_PATH];
 	snprintf(dllPath, sizeof(dllPath), "%s\\Revive\\%s\\%s", cwd, platform, dllName);
 	int dllPathLength = sizeof(dllPath);
+	return InjectDLL(processHandle, threadHandle, dllPath, dllPathLength);
+}
 
+bool InjectLibRevive(HANDLE processHandle, HANDLE threadHandle) {
+	return InjectDLL(processHandle, threadHandle, "LibRevive32_1.dll", "LibRevive64_1.dll");
+}
+
+bool InjectOpenVR(HANDLE processHandle, HANDLE threadHandle) {
+	const char *dllName = "openvr_api.dll";
+	return InjectDLL(processHandle, threadHandle, dllName, dllName);
+}
+
+bool InjectDLL(HANDLE processHandle, HANDLE threadHandle, const char *dllPath, int dllPathLength)
+{
 	HMODULE hModule = GetModuleHandle(L"kernel32.dll");
 	LPVOID loadLibraryAddr = (LPVOID)GetProcAddress(hModule, "LoadLibraryA");
 	if (loadLibraryAddr == NULL)
 	{
 		printf("Unable to locate LoadLibraryA\n");
-		return -1;
+		return false;
 	}
-	LPVOID loadLibraryArg = (LPVOID)VirtualAllocEx(pi.hProcess, NULL, dllPathLength, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	LPVOID loadLibraryArg = (LPVOID)VirtualAllocEx(processHandle, NULL, dllPathLength, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 	if (loadLibraryArg == NULL)
 	{
-		printf("Could not allocate memory in process %u\n", pi.dwProcessId);
-		return -1;
+		printf("Could not allocate memory in process\n");
+		return false;
 	}
 
-	int n = WriteProcessMemory(pi.hProcess, loadLibraryArg, dllPath, dllPathLength, NULL);
+	int n = WriteProcessMemory(processHandle, loadLibraryArg, dllPath, dllPathLength, NULL);
 	if (n == 0)
 	{
 		printf("Could not write to process's address space\n");
-		return -1;
+		return false;
 	}
 
-	HANDLE hThread = CreateRemoteThread(pi.hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddr, loadLibraryArg, NULL, NULL);
+	HANDLE hThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddr, loadLibraryArg, NULL, NULL);
 	if (hThread == INVALID_HANDLE_VALUE)
 	{
 		printf("Failed to create remote thread in process\n");
-		return -1;
+		return false;
 	}
 
 	DWORD waitReturnValue = WaitForSingleObject(hThread, INFINITE);
 	if (waitReturnValue != WAIT_OBJECT_0) {
 		printf("Failed to wait for LoadLibrary to exit\n");
-		return -1;
+		return false;
 	}
 	DWORD loadLibraryReturnValue;
 	GetExitCodeThread(hThread, &loadLibraryReturnValue);
 	if (loadLibraryReturnValue == NULL) {
 		printf("LoadLibrary failed to return module handle\n");
-		return -1;
+		return false;
 	}
-
-	printf("Injected dll succesfully\n");
-	ResumeThread(pi.hThread);
-	return 0;
+	return true;
 }
--- ReviveInjector/ReviveInject.h
+++ ReviveInjector/ReviveInject.h
@@ -1,1 +1,9 @@
+/* 
+Note: The files should be arranged as follows:
+   ReviveInjector.exe
+   Revive\x86\openvr_api.dll
+   Revive\x86\LibRevive32_1.dll
+   Revive\x64\openvr_api.dll
+   Revive\x64\LibRevive64_1.dll
+*/
 int CreateProcessAndInject(char *programPath);
