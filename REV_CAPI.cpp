56c3b57beb674d327abfb050510f580209f8bb5c
LibreVR <librevr@users.noreply.github.com>
2016-04-13 10:18:25 +0000

    ovr_SetControllerVibration: Add Oculus Touch support.

--- REV_CAPI.cpp
+++ REV_CAPI.cpp
@@ -546,7 +546,6 @@ OVR_PUBLIC_FUNCTION(unsigned int) ovr_GetConnectedControllerTypes(ovrSession ses
 OVR_PUBLIC_FUNCTION(ovrResult) ovr_SetControllerVibration(ovrSession session, ovrControllerType controllerType, float frequency, float amplitude)
 {
 	// TODO: Disable the rumbler after a nominal amount of time.
-	// TODO: Implement Oculus Touch support.
 
 	if (controllerType == ovrControllerType_XBox)
 	{
@@ -563,6 +562,28 @@ OVR_PUBLIC_FUNCTION(ovrResult) ovr_SetControllerVibration(ovrSession session, ov
 		XInputSetState(0, &vibration);
 
 		return ovrSuccess;
+	}
+
+	if (controllerType == ovrControllerType_LTouch)
+	{
+		vr::TrackedDeviceIndex_t index = g_VRSystem->GetTrackedDeviceIndexForControllerRole(vr::TrackedControllerRole_LeftHand);
+
+		if (index != (uint32_t)-1)
+		{
+			g_VRSystem->TriggerHapticPulse(index, 0, 1000.0f * amplitude);
+			return ovrSuccess;
+		}
+	}
+
+	if (controllerType == ovrControllerType_RTouch)
+	{
+		vr::TrackedDeviceIndex_t index = g_VRSystem->GetTrackedDeviceIndexForControllerRole(vr::TrackedControllerRole_RightHand);
+
+		if (index != (uint32_t)-1)
+		{
+			g_VRSystem->TriggerHapticPulse(index, 0, 1000.0f * amplitude);
+			return ovrSuccess;
+		}
 	}
 
 	return ovrError_DeviceUnavailable;
