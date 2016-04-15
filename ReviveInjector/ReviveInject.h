100db40f5cd25bb4e362e13e882510548a866dea
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2016-04-14 23:06:12 -0400

    Add injection support for Unity games

--- ReviveInjector/ReviveInject.h
+++ ReviveInjector/ReviveInject.h
@@ -3,7 +3,11 @@ Note: The files should be arranged as follows:
    ReviveInjector.exe
    Revive\x86\openvr_api.dll
    Revive\x86\LibRevive32_1.dll
+   Revive\x86\OVRPlugin.dll
    Revive\x64\openvr_api.dll
    Revive\x64\LibRevive64_1.dll
+   Revive\x64\OVRPlugin.dll
+
+   The OVRPlugin and openvr_api dlls are located in the 'Revive' folder in the solution root
 */
 int CreateProcessAndInject(char *programPath);
