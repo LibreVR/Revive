0c6f9bc1e93ad01be05c834e4b182a95eccea2c3
LibreVR <librevr@users.noreply.github.com>
2016-04-27 15:44:47 +0000

    Start implementation of the OpenGL backend.

--- REV_CAPI_GL.cpp
+++ REV_CAPI_GL.cpp
@@ -1,10 +1,28 @@
 #include "OVR_CAPI_GL.h"
 
+#include "openvr.h"
+#include <GL/glew.h>
+
 #include "REV_Assert.h"
+
+GLboolean glewInitialized = GL_FALSE;
 
 OVR_PUBLIC_FUNCTION(ovrResult) ovr_CreateTextureSwapChainGL(ovrSession session,
                                                             const ovrTextureSwapChainDesc* desc,
-                                                            ovrTextureSwapChain* out_TextureSwapChain) { REV_UNIMPLEMENTED_RUNTIME; }
+                                                            ovrTextureSwapChain* out_TextureSwapChain)
+{
+	if (!glewInitialized)
+	{
+		glewExperimental = GL_TRUE;
+		GLenum nGlewError = glewInit();
+		if (nGlewError != GLEW_OK)
+			return ovrError_RuntimeException;
+		glGetError(); // to clear the error caused deep in GLEW
+		glewInitialized = GL_TRUE;
+	}
+
+	REV_UNIMPLEMENTED_RUNTIME;
+}
 
 OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetTextureSwapChainBufferGL(ovrSession session,
                                                                ovrTextureSwapChain chain,
@@ -13,7 +31,20 @@ OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetTextureSwapChainBufferGL(ovrSession sessio
 
 OVR_PUBLIC_FUNCTION(ovrResult) ovr_CreateMirrorTextureGL(ovrSession session,
                                                          const ovrMirrorTextureDesc* desc,
-                                                         ovrMirrorTexture* out_MirrorTexture) { REV_UNIMPLEMENTED_RUNTIME; }
+                                                         ovrMirrorTexture* out_MirrorTexture)
+{
+	if (!glewInitialized)
+	{
+		glewExperimental = GL_TRUE;
+		GLenum nGlewError = glewInit();
+		if (nGlewError != GLEW_OK)
+			return ovrError_RuntimeException;
+		glGetError(); // to clear the error caused deep in GLEW
+		glewInitialized = GL_TRUE;
+	}
+
+	REV_UNIMPLEMENTED_RUNTIME;
+}
 
 OVR_PUBLIC_FUNCTION(ovrResult) ovr_GetMirrorTextureBufferGL(ovrSession session,
                                                             ovrMirrorTexture mirrorTexture,
