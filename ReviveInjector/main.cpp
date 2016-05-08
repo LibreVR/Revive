81d7c41b27c50a962c69574a155676b92b619653
LibreVR <librevr@users.noreply.github.com>
2016-05-08 14:42:54 +0000

    ReviveInjector: Prepend the process path with the base path.

--- ReviveInjector/main.cpp
+++ ReviveInjector/main.cpp
@@ -1,14 +1,26 @@
+#include <Windows.h>
 #include <stdio.h>
 #include <string.h>
 #include "ReviveInject.h"
 
-int main(int argc, char *argv[]) {
+int wmain(int argc, wchar_t *argv[]) {
 	if (argc < 2) {
 		printf("usage: ReviveInjector.exe [/handle] <process path/process handle>\n");
 		return -1;
 	}
-	if (strcmp(argv[1], "/handle") == 0 && argc > 2)
+	if (wcscmp(argv[1], L"/handle") == 0 && argc > 2)
 		return OpenProcessAndInject(argv[2]);
-	else
-		return CreateProcessAndInject(argv[1]);
+
+	// Replace all forward slashes with backslashes in the path
+	for (wchar_t* ptr = argv[1]; *ptr != L'\0'; ptr++)
+		if (*ptr == L'/') *ptr = L'\\';
+
+	// Prepend the base path
+	DWORD size = MAX_PATH;
+	WCHAR path[MAX_PATH];
+	HKEY oculusKey;
+	RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Software\\Oculus VR, LLC\\Oculus", 0, KEY_READ | KEY_WOW64_32KEY, &oculusKey);
+	RegQueryValueEx(oculusKey, L"Base", NULL, NULL, (PBYTE)path, &size);
+	wcsncat(path, argv[1], MAX_PATH);
+	return CreateProcessAndInject(path);
 }
