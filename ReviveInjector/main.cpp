c1f6011e969a549ef0cb47844ffcc0d04481cd52
LibreVR <librevr@users.noreply.github.com>
2016-05-10 20:58:35 +0000

    Revert "ReviveInjector: Add argument for base path prefix functionality."
    This reverts commit 70ee5dee4df92fdec2af5fbfa15093011a71a367.

--- ReviveInjector/main.cpp
+++ ReviveInjector/main.cpp
@@ -5,31 +5,22 @@
 
 int wmain(int argc, wchar_t *argv[]) {
 	if (argc < 2) {
-		printf("usage: ReviveInjector.exe [/handle|/base] <process path/process handle>\n");
+		printf("usage: ReviveInjector.exe [/handle] <process path/process handle>\n");
 		return -1;
 	}
 	if (wcscmp(argv[1], L"/handle") == 0 && argc > 2)
 		return OpenProcessAndInject(argv[2]);
 
+	// Replace all forward slashes with backslashes in the path
+	for (wchar_t* ptr = argv[1]; *ptr != L'\0'; ptr++)
+		if (*ptr == L'/') *ptr = L'\\';
+
+	// Prepend the base path
+	DWORD size = MAX_PATH;
 	WCHAR path[MAX_PATH];
-	if (wcscmp(argv[1], L"/base") == 0 && argc > 2)
-	{
-		// Replace all forward slashes with backslashes in the path
-		for (wchar_t* ptr = argv[2]; *ptr != L'\0'; ptr++)
-			if (*ptr == L'/') *ptr = L'\\';
-
-		// Prepend the base path
-		DWORD size = MAX_PATH;
-		HKEY oculusKey;
-		RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Software\\Oculus VR, LLC\\Oculus", 0, KEY_READ | KEY_WOW64_32KEY, &oculusKey);
-		RegQueryValueEx(oculusKey, L"Base", NULL, NULL, (PBYTE)path, &size);
-		RegCloseKey(oculusKey);
-		wcsncat(path, argv[2], MAX_PATH);
-	}
-	else
-	{
-		wcsncpy(path, argv[1], MAX_PATH);
-	}
-
+	HKEY oculusKey;
+	RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Software\\Oculus VR, LLC\\Oculus", 0, KEY_READ | KEY_WOW64_32KEY, &oculusKey);
+	RegQueryValueEx(oculusKey, L"Base", NULL, NULL, (PBYTE)path, &size);
+	wcsncat(path, argv[1], MAX_PATH);
 	return CreateProcessAndInject(path);
 }
