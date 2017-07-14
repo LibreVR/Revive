11ab6f6250d1357ff5f8a92ceab1c435468d0861
Jules Blok <jules.blok@gmail.com>
2017-07-14 22:09:17 +0200

    Implement some private APIs for Oculus Medium.

--- Revive/REV_CAPI.cpp
+++ Revive/REV_CAPI.cpp
@@ -1108,3 +1108,26 @@ OVR_PUBLIC_FUNCTION(ovrResult) ovr_SetExternalCameraProperties(ovrSession sessio
 {
 	return ovrError_NoExternalCameraInfo;
 }
+
+OVR_PUBLIC_FUNCTION(unsigned int) ovr_GetEnabledCaps(ovrSession session)
+{
+	return 0;
+}
+
+OVR_PUBLIC_FUNCTION(void) ovr_SetEnabledCaps(ovrSession session, unsigned int hmdCaps)
+{
+}
+
+OVR_PUBLIC_FUNCTION(unsigned int) ovr_GetTrackingCaps(ovrSession session)
+{
+	return 0;
+}
+
+OVR_PUBLIC_FUNCTION(ovrResult)
+ovr_ConfigureTracking(
+	ovrSession session,
+	unsigned int requestedTrackingCaps,
+	unsigned int requiredTrackingCaps)
+{
+	return ovrSuccess;
+}
