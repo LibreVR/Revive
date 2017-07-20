e5820d22b65d399bb63c9c259a8754d9a4ec76dc
Jules Blok <jules.blok@gmail.com>
2017-07-20 16:31:57 +0200

    Vulkan: Implement ovr_GetSessionPhysicalDeviceVk.

--- Revive/REV_CAPI_Vk.cpp
+++ Revive/REV_CAPI_Vk.cpp
@@ -1,5 +1,6 @@
 #include <vulkan/vulkan.h>
 #include <openvr.h>
+#include <vector>
 
 #include "OVR_CAPI_Vk.h"
 #include "Assert.h"
@@ -16,8 +17,48 @@ ovr_GetSessionPhysicalDeviceVk(
 	if (!out_physicalDevice)
 		return ovrError_InvalidParameter;
 
-	vr::VRSystem()->GetOutputDevice((uint64_t*)out_physicalDevice, vr::TextureType_Vulkan);
-	return ovrSuccess;
+	uint32_t gpu_count;
+	std::vector<VkPhysicalDevice> devices;
+
+	PFN_vkGetPhysicalDeviceProperties2KHR GetPhysicalDeviceProperties2KHR =
+		(PFN_vkGetPhysicalDeviceProperties2KHR)vkGetInstanceProcAddr(instance, "vkGetPhysicalDeviceProperties2KHR");
+
+	// Get the number of devices (GPUs) available.
+	VkResult res = vkEnumeratePhysicalDevices(instance, &gpu_count, NULL);
+	// Allocate space and get the list of devices.
+	devices.resize(gpu_count);
+	res = vkEnumeratePhysicalDevices(instance, &gpu_count, devices.data());
+
+	VkPhysicalDevice physicalDevice = 0;
+#if 0
+	// TODO: We could request this directly from OpenVR, but it seems to always return 0.
+	vr::VRSystem()->GetOutputDevice((uint64_t*)&physicalDevice, vr::TextureType_Vulkan);
+#else
+	for (VkPhysicalDevice device : devices)
+	{
+		VkPhysicalDeviceIDPropertiesKHR uid = {};
+		uid.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_ID_PROPERTIES_KHR;
+		VkPhysicalDeviceProperties2KHR properties = {};
+		properties.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2_KHR;
+		properties.pNext = &uid;
+
+		GetPhysicalDeviceProperties2KHR(device, &properties);
+
+		if (!uid.deviceLUIDValid)
+			continue;
+
+		if (memcmp(uid.deviceLUID, luid.Reserved, VK_LUID_SIZE_KHR) == 0)
+		{
+			physicalDevice = device;
+			break;
+		}
+	}
+#endif
+
+	if (out_physicalDevice)
+		*out_physicalDevice = physicalDevice;
+
+	return physicalDevice ? ovrSuccess : ovrError_MismatchedAdapters;
 }
 
 OVR_PUBLIC_FUNCTION(ovrResult) ovr_SetSynchonizationQueueVk(ovrSession session, VkQueue queue)
