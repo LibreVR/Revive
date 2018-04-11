530d6dfd6df400eb99054e462cea9d246674bcb7
Jules Blok <jules.blok@gmail.com>
2018-04-11 16:43:58 +0200

    ReviveOverlay: Use raw string literals.

--- ReviveOverlay/revivemanifestcontroller.cpp
+++ ReviveOverlay/revivemanifestcontroller.cpp
@@ -116,10 +116,10 @@ CReviveManifestController::CReviveManifestController()
 	, m_defaultsFile(QString(vr::VR_RuntimePath()) + "/resources/settings/default.vrsettings")
 	, m_bLibraryFound(false)
 {
-	m_supportArgs["revive.app.oculus-dreamdeck-nux"] = "/base Support\\oculus-dreamdeck-nux\\Dreamdeck\\Binaries\\Win64\\Dreamdeck-Win64-Shipping.exe -vr -dreamdeck=NUX";
-	m_supportArgs["revive.app.oculus-touch-tutorial"] = "/base Support\\oculus-touch-tutorial\\TouchNUX\\Binaries\\Win64\\TouchNUX-Win64-Shipping.exe -gamemode=nux";
-	m_supportArgs["revive.app.oculus-first-contact"] = "/base Support\\oculus-touch-tutorial\\TouchNUX\\Binaries\\Win64\\TouchNUX-Win64-Shipping.exe -gamemode=\"experienceonly\"";
-	m_supportArgs["revive.app.oculus-avatar-editor"] = "/base Support\\oculus-avatar-editor\\OVRAvatarEditor.exe";
+	m_supportArgs["revive.app.oculus-dreamdeck-nux"] = R"(/base Support\oculus-dreamdeck-nux\Dreamdeck\Binaries\Win64\Dreamdeck-Win64-Shipping.exe -vr -dreamdeck=NUX)";
+	m_supportArgs["revive.app.oculus-touch-tutorial"] = R"(/base Support\oculus-touch-tutorial\TouchNUX\Binaries\Win64\TouchNUX-Win64-Shipping.exe -gamemode=nux)";
+	m_supportArgs["revive.app.oculus-first-contact"] = R"(/base Support\oculus-touch-tutorial\TouchNUX\Binaries\Win64\TouchNUX-Win64-Shipping.exe -gamemode="experienceonly")";
+	m_supportArgs["revive.app.oculus-avatar-editor"] = R"(/base Support\oculus-avatar-editor\OVRAvatarEditor.exe)";
 }
 
 CReviveManifestController::~CReviveManifestController()
