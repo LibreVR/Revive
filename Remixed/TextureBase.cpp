b48afb60caa3fea7295c47a70c39b5132c2e4569
Jules Blok <jules.blok@gmail.com>
2018-05-18 16:11:44 +0200

    Remixed: Treat R11G11B10_FLOAT as an SRGB format.

--- Remixed/TextureBase.cpp
+++ Remixed/TextureBase.cpp
@@ -13,15 +13,16 @@ bool TextureBase::IsSRGBFormat(ovrTextureFormat format)
 {
 	switch (format)
 	{
-		case OVR_FORMAT_R8G8B8A8_UNORM_SRGB:  return true;
-		case OVR_FORMAT_B8G8R8A8_UNORM_SRGB:  return true;
-		case OVR_FORMAT_B8G8R8X8_UNORM_SRGB:  return true;
+		case OVR_FORMAT_R8G8B8A8_UNORM_SRGB:	return true;
+		case OVR_FORMAT_B8G8R8A8_UNORM_SRGB:	return true;
+		case OVR_FORMAT_B8G8R8X8_UNORM_SRGB:	return true;
+		case OVR_FORMAT_R11G11B10_FLOAT:		return true;
 
 		// Added in 1.5 compressed formats can be used for static layers
-		case OVR_FORMAT_BC1_UNORM_SRGB:       return true;
-		case OVR_FORMAT_BC2_UNORM_SRGB:       return true;
-		case OVR_FORMAT_BC3_UNORM_SRGB:       return true;
-		case OVR_FORMAT_BC7_UNORM_SRGB:       return true;
+		case OVR_FORMAT_BC1_UNORM_SRGB:			return true;
+		case OVR_FORMAT_BC2_UNORM_SRGB:			return true;
+		case OVR_FORMAT_BC3_UNORM_SRGB:			return true;
+		case OVR_FORMAT_BC7_UNORM_SRGB:			return true;
 
 		default: return false;
 	}
@@ -31,10 +32,10 @@ bool TextureBase::IsDepthFormat(ovrTextureFormat format)
 {
 	switch (format)
 	{
-		case OVR_FORMAT_D16_UNORM:            return true;
-		case OVR_FORMAT_D24_UNORM_S8_UINT:    return true;
-		case OVR_FORMAT_D32_FLOAT:            return true;
-		case OVR_FORMAT_D32_FLOAT_S8X24_UINT: return true;
+		case OVR_FORMAT_D16_UNORM:				return true;
+		case OVR_FORMAT_D24_UNORM_S8_UINT:		return true;
+		case OVR_FORMAT_D32_FLOAT:				return true;
+		case OVR_FORMAT_D32_FLOAT_S8X24_UINT:	return true;
 
 		default: return false;
 	}
