07c39fc04b06c81e6e9bf8be0f51384ff70d6c30
Jules Blok <jules.blok@gmail.com>
2019-08-05 01:36:43 +0100

    HapticsBuffer: Lead by write index instead of read index.

--- Revive/HapticsBuffer.cpp
+++ Revive/HapticsBuffer.cpp
@@ -1,7 +1,7 @@
 #include "HapticsBuffer.h"
 
 HapticsBuffer::HapticsBuffer()
-	: m_ReadIndex(0)
+	: m_ReadIndex(0xFF)
 	, m_WriteIndex(0)
 	, m_Buffer()
 {
@@ -16,7 +16,7 @@ void HapticsBuffer::AddSamples(const ovrHapticsBuffer* buffer)
 
 	for (int i = 0; i < buffer->SamplesCount; i++)
 	{
-		if (m_WriteIndex == m_ReadIndex - 1)
+		if (m_WriteIndex == m_ReadIndex)
 			return;
 
 		m_Buffer[m_WriteIndex] = samples[i];
@@ -52,14 +52,11 @@ float HapticsBuffer::GetSample()
 	}
 
 	// We can't pass the write index, so the buffer is now empty
-	if (m_ReadIndex == m_WriteIndex)
+	if (m_ReadIndex + 1 == m_WriteIndex)
 		return 0.0f;
 
-	uint8_t sample = m_Buffer[m_ReadIndex];
-
-	// We increment the atomic here as a memory barrier
 	// Index will overflow correctly, so no need for a modulo operator
-	m_ReadIndex++;
+	uint8_t sample = m_Buffer[++m_ReadIndex];
 
 	return sample / 255.0f;
 }
@@ -70,9 +67,11 @@ ovrHapticsPlaybackState HapticsBuffer::GetState()
 
 	for (uint8_t i = m_WriteIndex; i != m_ReadIndex; i++)
 		state.RemainingQueueSpace++;
+	state.RemainingQueueSpace++;
 
 	for (uint8_t i = m_ReadIndex; i != m_WriteIndex; i++)
 		state.SamplesQueued++;
+	state.SamplesQueued--;
 
 	return state;
 }
