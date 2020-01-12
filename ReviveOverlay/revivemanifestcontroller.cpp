56c9f1b920ac0c3277a56dc3e9040588618fb3a8
christophe roger <croger42@gmail.com>
2020-01-13 00:01:47 +0100

    cleaning code

--- ReviveOverlay/revivemanifestcontroller.cpp
+++ ReviveOverlay/revivemanifestcontroller.cpp
@@ -74,39 +74,13 @@ void QueryKey(HKEY hKey, QStringList &path_array)
 {
 	TCHAR    achKey[MAX_KEY_LENGTH];   // buffer for subkey name
 	DWORD    cbName;                   // size of name string 
-	TCHAR    achClass[MAX_PATH] = TEXT("");  // buffer for class name 
-	DWORD    cchClassName = MAX_PATH;  // size of class string 
 	DWORD    cSubKeys = 0;               // number of subkeys 
-	DWORD    cbMaxSubKey;              // longest subkey size 
-	DWORD    cchMaxClass;              // longest class string 
-	DWORD    cValues;              // number of values for key 
-	DWORD    cchMaxValue;          // longest value name 
-	DWORD    cbMaxValueData;       // longest value data 
-	DWORD    cbSecurityDescriptor; // size of security descriptor 
-	FILETIME ftLastWriteTime;      // last write time 
-
 	DWORD i, retCode;
 
-	TCHAR  achValue[MAX_VALUE_NAME];
-	DWORD cchValue = MAX_VALUE_NAME;
-
 	// Get the class name and the value count. 
-	retCode = RegQueryInfoKey(
-		hKey,                    // key handle 
-		achClass,                // buffer for class name 
-		&cchClassName,           // size of class string 
-		NULL,                    // reserved 
-		&cSubKeys,               // number of subkeys 
-		&cbMaxSubKey,            // longest subkey size 
-		&cchMaxClass,            // longest class string 
-		&cValues,                // number of values for this key 
-		&cchMaxValue,            // longest value name 
-		&cbMaxValueData,         // longest value data 
-		&cbSecurityDescriptor,   // security descriptor 
-		&ftLastWriteTime);       // last write time 
+	retCode = RegQueryInfoKey(hKey, NULL, NULL, NULL, /* number of subkeys*/ &cSubKeys, NULL, NULL, NULL, NULL, NULL, NULL, NULL); 
 
 	// Enumerate the subkeys, until RegEnumKeyEx fails.
-
 	if (cSubKeys)
 	{
 		WCHAR* paths = new WCHAR[cSubKeys];
@@ -120,7 +94,7 @@ void QueryKey(HKEY hKey, QStringList &path_array)
 				NULL,
 				NULL,
 				NULL,
-				&ftLastWriteTime);
+				NULL);
 			if (retCode == ERROR_SUCCESS)
 			{
 				wprintf(TEXT("(%d) %s\n"), i + 1, achKey);
