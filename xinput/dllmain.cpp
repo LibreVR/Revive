bfebaa3e8b294ec21a7966321a4f3e4cf0ced63e
Jules Blok <jules.blok@gmail.com>
2020-04-25 23:55:33 -0700

    xinput: Load Revive DLLs from install directory.

--- xinput/dllmain.cpp
+++ xinput/dllmain.cpp
@@ -1,5 +1,10 @@
 #include <Windows.h>
 
+bool GetReviveBasePath(PWCHAR path, DWORD length)
+{
+	return RegGetValueA(HKEY_LOCAL_MACHINE, "Software\\Revive", "", RRF_RT_REG_SZ | RRF_SUBKEY_WOW6432KEY, NULL, path, &length) == ERROR_SUCCESS;
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -9,11 +14,21 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
+		wchar_t basePath[MAX_PATH];
+		if (GetReviveBasePath(basePath, MAX_PATH))
+		{
 #if _WIN64
-		LoadLibrary(L"LibRevive64.dll");
+			wcscat_s(basePath, MAX_PATH, L"\\openvr_api64.dll");
+			LoadLibrary(basePath);
+			wcscpy_s(wcsrchr(basePath, '\\'), MAX_PATH, L"\\LibRevive64.dll");
+			LoadLibrary(basePath);
 #else
-		LoadLibrary(L"LibRevive32.dll");
+			wcscat_s(basePath, MAX_PATH, L"\\openvr_api32.dll");
+			LoadLibrary(basePath);
+			wcscpy_s(wcsrchr(basePath, '\\'), MAX_PATH, L"\\LibRevive32.dll");
+			LoadLibrary(basePath);
 #endif
+		}
 		break;
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
